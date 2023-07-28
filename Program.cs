754d5a63594589d39ba6e1ddd94b753f24de2557
unknown <vasanthgamer2000@gmail.com>
2023-07-28 17:56:11 +0530

    Chnages have been committed

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recursion
+            runApp();
+            Console.ReadKey();
+        }
+
+        public static void runApp()
+        {
+            string[] students = new string[10];
+            students[0] = "Karuna";
+            students[1] = "Mark";
+            students[2] = "Advaith";
+            students[3] = "Sangeeta";
+            students[4] = "Nazia";
+            students[5] = "Faisal";
+            students[6] = "Tania";
+            students[7] = "Guru";
+            students[8] = "Chandni";
+            students[9] = "Kamleshwar";
+
+            int currElem = 0;
+            printViaRecursion(students, currElem);
+        }
+
+        public static void printViaRecursion(string[] arr, int currElem)
+        {
+            Console.WriteLine(arr[currElem]);
+            if (currElem < arr.Length - 1)
+                printViaRecursion(arr, currElem + 1);
+        }
+
+    }
+}
